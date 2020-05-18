c7c3b93c0eda5e474ededb3fb73b0f95aa503968
Fedoskina <fedosikhina@gmail.com>
2020-05-18 20:38:39 +0200

    m2t1: refactor

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -24,16 +24,36 @@ const getTypeIndex = () => {
   return Math.floor(Math.random() * offerType.length);
 };
 
-const generateDescription = (count) => (
-  Array(count).fill({}).map(() => ({
+const generateMock = (content) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, content, (err) => {
+      if (err) {
+        return reject(`Can't write data to file...`);
+      }
+
+      return resolve(`Operation success. File created.`);
+    });
+  });
+};
+
+const generateDescription = (count) => {
+  let mockData = [];
+  const generateEntry = () => ({
     category: [category[getRandomInt(0, category.length - 1)]],
     description: shuffle(description).slice(1, 5).join(` `),
     picture: getPictureFileName(),
     title: title[getRandomInt(0, title.length - 1)],
     type: offerType[getTypeIndex()],
     sum: getRandomInt(SumRestrict.min, SumRestrict.max),
-  }))
-);
+  });
+
+  for (let i = 0; i < count; i++) {
+    const mock = generateEntry();
+    mockData.push(mock);
+  }
+
+  return mockData;
+};
 
 module.exports = {
   name: `--generate`,
@@ -48,20 +68,8 @@ module.exports = {
     const countOffer = Number.parseInt(count, 10) || defaultAmount;
     const content = JSON.stringify(generateDescription(countOffer), null, 2);
 
-    const generateMock = () => {
-      return new Promise((resolve, reject) => {
-        fs.writeFile(fileName, content, (err) => {
-          if (err) {
-            return reject(`Can't write data to file...`);
-          }
-
-          return resolve(`Operation success. File created.`);
-        });
-      });
-    };
-
     try {
-      const successMessage = await generateMock();
+      const successMessage = await generateMock(content);
       console.log(successTheme(successMessage));
     } catch (e) {
       console.log(errorTheme(e));
